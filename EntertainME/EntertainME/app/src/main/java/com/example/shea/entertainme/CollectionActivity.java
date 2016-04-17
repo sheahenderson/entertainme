57908f8162390359c7dea50b4ae662eecac1b56a
Zackary Murphy <zmurphy1125@gmail.com>
2016-04-17 12:50:08 -0500

    Reimplemented adding and viewing books

--- EntertainME/EntertainME/app/src/main/java/com/example/shea/entertainme/CollectionActivity.java
+++ EntertainME/EntertainME/app/src/main/java/com/example/shea/entertainme/CollectionActivity.java
@@ -3,8 +3,6 @@ package com.example.shea.entertainme;
 import android.app.DialogFragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -12,7 +10,7 @@ import android.widget.Button;
 
 public class CollectionActivity extends AppCompatActivity
 implements ViewDialog.ViewDialogListener {
-
+    private int desire = Constants.INVALID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,16 +24,15 @@ implements ViewDialog.ViewDialogListener {
         addItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent nextScreen = new Intent(v.getContext(), AddMovieActivity.class);
-                startActivityForResult(nextScreen, 0);
+                desire = Constants.DESIRE_ADD;
+                showViewDialog();
             }
         });
         assert viewButton != null;
         viewButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Intent nextScreen = new Intent(v.getContext(), ViewActivity.class);
-                //startActivityForResult(nextScreen, 0);
+                desire = Constants.DESIRE_VIEW;
                 showViewDialog();
             }
         });
@@ -48,10 +45,24 @@ implements ViewDialog.ViewDialogListener {
 
     @Override
     public void onItemClick(DialogFragment dialog, int retVal) {
+        Intent intent;
         if (retVal != Constants.INVALID) {
-            Intent intent = new Intent(CollectionActivity.this, ViewActivity.class);
-            intent.putExtra(Constants.EXTRAS_VIEW, retVal);
-            startActivity(intent);
+            switch(desire) {
+                case Constants.DESIRE_ADD:
+                    intent = new Intent(CollectionActivity.this, AddActivity.class);
+                    intent.putExtra(Constants.EXTRAS_ADD, retVal);
+                    startActivity(intent);
+                    break;
+                case Constants.DESIRE_VIEW:
+                    intent = new Intent(CollectionActivity.this, ViewActivity.class);
+                    intent.putExtra(Constants.EXTRAS_VIEW, retVal);
+                    startActivity(intent);
+                    break;
+                default:
+                    //desire = Constants.INVALID;
+                    break;
+            }
         }
+        desire = Constants.INVALID;
     }
 }
